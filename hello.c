64e5c988815d115d509d0b0621f7afb89e94668f
Dawson <dgill8@unl.edu>
2025-08-28 10:14:52 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Dawson Gill
+ *         EMAIL
+ * Date: 08/28/2025
+ *
+ * Prints for Hack 1
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Dawson Gill!\n");
+    printf("My major is CS!\n");
+
+}
